6ee95dc70e582edb5d0ca5a720e8b3e98ae966c9
Vadim Korobka <34455@mail.ua>
2020-10-21 22:04:37 +0300

    Updates

---
+++ precache-manifest.38cfeb64ffcd41a3a3d4fc9739ffe788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262495644ba814b0d95fdc05698e859a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a71f3220504eb552bc3",
+    "url": "./static/css/main.38f3043c.chunk.css"
+  },
+  {
+    "revision": "dd9f4548ce254d15e18f",
+    "url": "./static/js/2.63cb416f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.63cb416f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a71f3220504eb552bc3",
+    "url": "./static/js/main.f868af1d.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
